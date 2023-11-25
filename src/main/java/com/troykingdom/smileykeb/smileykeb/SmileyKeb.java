7ffe80a641e114faa169aedb6caf37e0c1d1c220
Carl Justine Malabanan <carljustinem984@gmail.com>
2023-11-26 01:01:59 +0800

    created Files

---
+++ src/main/java/com/troykingdom/smileykeb/smileykeb/SmileyKeb.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.troykingdom.smileykeb.smileykeb;
+
+/**
+ *
+ * @author PC
+ */
+public class SmileyKeb {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
