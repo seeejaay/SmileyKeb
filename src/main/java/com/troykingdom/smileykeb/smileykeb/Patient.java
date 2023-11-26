62ba0ec8267a79319634e0d7a776752871c8a282
Carl Justine Malabanan <carljustinem984@gmail.com>
2023-11-26 21:58:10 +0800

    Created Patient File Info, Updated SmileyKeb

---
+++ src/main/java/com/troykingdom/smileykeb/smileykeb/Patient.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.troykingdom.smileykeb.smileykeb;
+
+/**
+ *
+ * @author PC
+ */
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
+public class Patient {
+    private String firstName;
+    private String lastName;
+    private int birthDay;
+    private String birthMonth;
+    private int birthyear;
+    private int contactNumber;
+    private String uName;
+    private Scanner scan;
+    public void setFName(String fName){
+        this.firstName = fName;
+    }
+    
+    public void setLname (String lName){
+        this.lastName = lName;
+    }
+    
+    public void setbirthDay(int bDay){
+        this.birthDay = bDay;
+    }
+    
+    public void setbirthMonth(String bMonth){
+        this.birthMonth = bMonth;
+    }
+    
+    public void setbirthYear(int bYear){
+        this.birthyear = bYear;
+    }
+    
+    public String getName(){
+        return firstName + " " + lastName;
+    }
+    
+    public String getbirthDay(){
+        String bday = birthMonth + " " + birthDay+ " " + birthyear; 
+        return bday;
+    }
+    
+    public int getconNumber(){
+        return contactNumber;
+    }
+    
+    public void setUName()
+    {
+        String userName = firstName.charAt(0) +lastName + String.valueOf(birthyear);
+        
+        this.uName = userName;
+    }
+    
+    public String getUName(){
+        return uName;
+    }
+    
+    public void savePatientDetails() {
+        String filePath = "PatientInfo/" + uName + ".txt";
+
+        try {
+            File file = new File(filePath);
+
+            if (file.exists()) {
+                System.out.println("Patient file already exists. Cannot overwrite.");
+                return;  // Return without saving if the file already exists
+            }
+            try (FileWriter fw = new FileWriter(filePath)) {
+                fw.write(getUName() + "\t" + getName() + "\t" + getbirthDay());
+                // Add more details as needed
+                System.out.println("Patient details saved to file.");
+            }
+        } catch (IOException e) {
+            System.out.println("Error saving patient details: " + e.getMessage());
+        }
+    }
+    
+    public void saveUserName(){
+        try {
+           FileWriter fw = new FileWriter("UserName/usernames.txt");
+           fw.write(getUName() + "\n");
+           fw.close();
+        }catch(IOException e){
+            System.out.println("Error Storing UserName" + e);
+        }
+    }
+    
+    
+    
+}
--- src/main/java/com/troykingdom/smileykeb/smileykeb/SmileyKeb.java
+++ src/main/java/com/troykingdom/smileykeb/smileykeb/SmileyKeb.java
@@ -1,12 +1,86 @@
 package com.troykingdom.smileykeb.smileykeb;
 import java.util.Scanner;
- 
+import java.io.File;
 public class SmileyKeb {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Appointment apt = new Appointment();
-        int choice;
-        do{
+            menu(scan);
+    }
+    
+    public static void menu(Scanner scan){
+        int inp;
+        System.out.println("Welcome to SmileyKeb Dental Clinic\n");
+        
+        System.out.println("------------MENU------------");
+        System.out.println("[1] Login");
+        System.out.println("[2] SignUp");
+        System.out.println("[3] Exit");
+        System.out.print("Choice: ");
+        inp = scan.nextInt();
+        scan.nextLine();
+        
+        switch(inp){
+            case 1: 
+                Login(scan);
+            break;
+            case 2:
+                signUp(scan);
+            break;
+            case 3:
+                System.exit(0);
+            default:
+                System.out.println("Invalid Input");
+                menu(scan);
+                break;
+        }
+        
+    }
+    
+    public static void Login(Scanner scan){
+        String uName;
+        
+        System.out.println("Hint: First letter of lastname and birthyear");
+        System.out.print("Enter your username: ");
+        uName = scan.nextLine();
+        File file = new File(uName + ".txt");
+        if (file.exists()) {
+            System.out.println("Login Successful!");
+            Appointment(scan);
+            // You can read the file to get patient details if needed
+        } else {
+            System.out.println("Username not found. Please sign up.");
+        }
+        
+    }
+    
+    public static void signUp(Scanner scan){
+        
+        Patient newPat = new Patient();
+        System.out.print("Enter your FirstName: ");
+        newPat.setFName(scan.nextLine());
+        
+        System.out.print("Enter your Last Name: ");
+        newPat.setLname(scan.nextLine());
+        
+        System.out.print("Enter your BirthDate: ");
+        newPat.setbirthDay(scan.nextInt());
+        
+        scan.nextLine();
+        System.out.print("Enter your BirthMonth: ");
+        newPat.setbirthMonth(scan.nextLine());
+        
+        System.out.print("Enter your BirthYear: ");
+        newPat.setbirthYear(scan.nextInt());
+        newPat.setUName();
+        System.out.println("Your username is " + newPat.getUName() + "\n");
+        
+        newPat.savePatientDetails();
+        newPat.saveUserName();
+        menu(scan);
+    }
+    
+    public static void Appointment(Scanner scan){
+            Appointment apt = new Appointment();
             System.out.println("\n------------MENU------------");
             System.out.println("[1] Book Appointment");
             System.out.println("[2] Open Existing Appointment");
@@ -15,7 +89,7 @@ public class SmileyKeb {
             System.out.println("----------------------------");
             
             System.out.print("Enter choice: ");
-            choice = scan.nextInt();
+            int choice = scan.nextInt();
             scan.nextLine();
             System.out.println("----------------------------");
             
@@ -26,15 +100,17 @@ public class SmileyKeb {
                 case 2:
                     apt.openAppointment(scan);
                     break;
-                /*case 3:
-                    apt.cancelAppointment(scan);
-                    break;*/
                 case 3:
+                    //apt.cancelAppointment(scan);
+                    break;
+                case 4:
                     System.exit(0);
                     break;
                 default:
                     System.out.println("Invalid Choice. Kindly enter again.");
+                    Appointment(scan);
+                    
             }
-        }while (choice >= 1 && choice <= 3);
     }
 }
+
